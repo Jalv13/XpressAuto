48b37884970a984ca694ab9de1a342a1889d9ffe
StrawBerryGelato <strawberrygelatos@gmail.com>
2025-03-06 09:50:51 -0500

    Oil and Tire pages slight update
    changed allign

--- frontend/xpress-app/src/Components/OilChange.jsx
+++ frontend/xpress-app/src/Components/OilChange.jsx
@@ -11,17 +11,21 @@ const OilChange = () => {
         position: 'relative',
         width: '100%',
         height: '500px',
-        backgroundImage: 'url(/images/oilchange1.jpg)',
+        backgroundImage: 'url(/images/oilchange2.jpg)',
         backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundPosition: 'relative',
         display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         color: '#fff',
         textAlign: 'center',
             }}>
                 <h1>Oil Change Services</h1>
-                <p>Welcome to our oil change services page. We offer a variety of oil change options to keep your vehicle running smoothly.</p>
+                <p>Welcome to our oil change services page. .</p>
+                <p>We offer a variety of oil change options to keep your vehicle running smoothly.</p>
+                <p>Synthethic and Regular Oil</p>
+                <p>Filter Changes are available as well</p>
             </div>
             <Footer />
         </>
--- frontend/xpress-app/src/Components/TireService.jsx
+++ frontend/xpress-app/src/Components/TireService.jsx
@@ -5,22 +5,25 @@ import Footer from "./Footer";
 const TireService = () => {
     return (
         <>
-            <Header />
-            <div className="tire-service" 
+             <Header />
+            <div className="hero" 
         style={{
         position: 'relative',
         width: '100%',
         height: '500px',
         backgroundImage: 'url(/images/tirechange1.jpg)',
         backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundPosition: 'relative',
         display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         color: '#fff',
         textAlign: 'center',
             }}>
                 <h1>Tire Services</h1>
+                <p>All Major Tire Brands Available</p>
+                <p>GoodYear,Michellin,Firestone</p>
                 
             </div>
             <Footer />
