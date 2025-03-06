c52342641aeeab9aa80e57d997928082325d2e14
Michael Coulter <coulte12@go.stockton.edu>
2025-03-05 22:32:43 -0500

    toying with design

--- frontend/xpress-app/src/App.jsx
+++ frontend/xpress-app/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./Components/Dashboard.jsx";
 import ForgotPassword from "./Components/ForgotPassword.jsx";
 import ResetPassword from "./Components/ResetPassword.jsx";
 import Profile from "./Components/Profile.jsx";
+import OilChange from "./Components/OilChange.jsx";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/services/oil-change" element={<OilChange />} />
         </Routes>
       </Router>
     </AuthProvider>
--- frontend/xpress-app/src/Components/OilChange.jsx
+++ frontend/xpress-app/src/Components/OilChange.jsx
@@ -1,14 +1,15 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import background from "../Components/oilchange1.jpg";
+import React from "react";
+import background from "../Components/images/Oilchange1demo.jpg";
 
 const OilChange = () => {
     return (
         <>
         <Header />
-        <div style={{backgroundImage: `url(${background})`, backgroundSize: "stretch", height: "100vh", color: "Black", textAlign: "center"}}>
-            <h1>Oil Change Services</h1>
-            <p>Welcome to our oil change services page. We offer a variety of oil change options to keep your vehicle running smoothly.</p>
+        <div style={{backgroundImage: `url(${background})`, backgroundSize: "cover", height: "10%", color: "Black", textAlign: "left", padding: "20px"}}>
+            <h1 style={{ backgroundColor: 'white' }}>Oil Change Services</h1>
+            <p style= {{ backgroundColor: 'white'}}>Welcome to our oil change services page. We offer a variety of oil change options to keep your vehicle running smoothly.</p>
             {/* Add more content and components as needed */}
         </div>
             <Footer />
