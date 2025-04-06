d5865fa9f1368491f2fb483de87067f48c5102b3
Joshua Alvarez <141381671+Jalv13@users.noreply.github.com>
2025-04-05 23:36:22 -0400

    CSS is gross, but working

--- frontend/xpress-app/src/Components/WhyUs.jsx
+++ frontend/xpress-app/src/Components/WhyUs.jsx
@@ -1,12 +1,16 @@
 //Authors: Joshua, Joe, , , ,
 
+//DELETE
+
 function WhyUs() {
   return (
     <section className="why-us">
       <h2>Why Choose Us?</h2>
       <div className="testimonials">
         <div className="testimonial">🏅 Always Professional</div>
-        <div className="testimonial">🏅 Serving Atlantic County for over 30 Years</div>
+        <div className="testimonial">
+          🏅 Serving Atlantic County for over 30 Years
+        </div>
         <div className="testimonial">🏅 Highly Recommended</div>
       </div>
     </section>
